a52fd10daf3b5fcc79cb1df4e7765106fa7d2947
bagus wardana <bagusredmi@gmail.com>
2025-04-25 20:13:34 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MyMusic
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListView listViewSongs;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listViewSongs = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // ListViewSongs
+            this.listViewSongs.Location = new System.Drawing.Point(12, 12);
+            this.listViewSongs.Size = new System.Drawing.Size(400, 200);
+            this.listViewSongs.View = System.Windows.Forms.View.List;
+
+            // btnAdd
+            this.btnAdd.Location = new System.Drawing.Point(420, 12);
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+
+            // btnEdit
+            this.btnEdit.Location = new System.Drawing.Point(420, 41);
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+
+            // btnDelete
+            this.btnDelete.Location = new System.Drawing.Point(420, 70);
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+
+            // btnPlay
+            this.btnPlay.Location = new System.Drawing.Point(420, 99);
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.Text = "Play";
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+
+            // btnStop
+            this.btnStop.Location = new System.Drawing.Point(420, 128);
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.Text = "Stop";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(520, 230);
+            this.Controls.Add(this.listViewSongs);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnStop);
+            this.Text = "MyMusic";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using NAudio.Wave;
+using MySql.Data.MySqlClient;
+
+namespace MyMusic
+{
+    public partial class Form1 : Form
+    {
+        private WaveOutEvent waveOut;        // Objek untuk memutar audio
+        private AudioFileReader audioFileReader; // Objek untuk membaca file audio
+        private string currentlyPlayingFile; // Menyimpan file yang sedang diputar
+        private string connectionString = "Server=localhost;Database=mymusic_db;Uid=root;Pwd=;"; // Sesuaikan dengan koneksi Anda
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSongsFromDatabase();
+        }
+
+        private void LoadSongsFromDatabase()
+        {
+            listViewSongs.Items.Clear();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT filepath FROM songs";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listViewSongs.Items.Add(reader.GetString("filepath"));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading songs: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Audio Files|*.mp3;*.wav";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileName = Path.GetFileName(filePath);
+
+                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            string query = "INSERT INTO songs (title, filepath) VALUES (@title, @filepath)";
+                            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                            {
+                                cmd.Parameters.AddWithValue("@title", fileName);
+                                cmd.Parameters.AddWithValue("@filepath", filePath);
+                                cmd.ExecuteNonQuery();
+                            }
+                            listViewSongs.Items.Add(filePath);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error adding song: " + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (listViewSongs.SelectedItems.Count > 0)
+            {
+                string oldFilePath = listViewSongs.SelectedItems[0].Text;
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Audio Files|*.mp3;*.wav";
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string newFilePath = openFileDialog.FileName;
+
+                        using (MySqlConnection connection = new MySqlConnection(connectionString))
+                        {
+                            try
+                            {
+                                connection.Open();
+                                string query = "UPDATE songs SET filepath = @filepath WHERE filepath = @oldFilePath";
+                                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                                {
+                                    cmd.Parameters.AddWithValue("@filepath", newFilePath);
+                                    cmd.Parameters.AddWithValue("@oldFilePath", oldFilePath);
+                                    cmd.ExecuteNonQuery();
+                                }
+                                listViewSongs.SelectedItems[0].Text = newFilePath;
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Error editing song: " + ex.Message);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih lagu yang ingin diedit.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listViewSongs.SelectedItems.Count > 0)
+            {
+                string filePath = listViewSongs.SelectedItems[0].Text;
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        string query = "DELETE FROM songs WHERE filepath = @filepath";
+                        using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@filepath", filePath);
+                            cmd.ExecuteNonQuery();
+                        }
+                        listViewSongs.Items.Remove(listViewSongs.SelectedItems[0]);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error deleting song: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih lagu yang ingin dihapus.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (listViewSongs.SelectedItems.Count > 0)
+            {
+                string filePath = listViewSongs.SelectedItems[0].Text;
+
+                if (File.Exists(filePath))
+                {
+                    StopAudio();
+
+                    currentlyPlayingFile = filePath;
+                    waveOut = new WaveOutEvent();
+                    audioFileReader = new AudioFileReader(filePath);
+                    waveOut.Init(audioFileReader);
+                    waveOut.Play();
+                }
+                else
+                {
+                    MessageBox.Show("File tidak ditemukan.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih lagu yang ingin diputar.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopAudio();
+        }
+
+        private void StopAudio()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            if (audioFileReader != null)
+            {
+                audioFileReader.Dispose();
+                audioFileReader = null;
+            }
+
+            currentlyPlayingFile = null;
+        }
+    }
+}
