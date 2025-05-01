1c0e6a6a5ff7a69448ab80ec533aae70e128210e
sipalingbagus <113612964+sipalingbagus@users.noreply.github.com>
2025-05-02 00:00:12 +0700

    Add files via upload
    Adding CRUD file tersendiri beda dengan form utama yang aplikasi music menggunakan CRUD juga

---
+++ CRUDForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace MyMusicApp
+{
+    partial class CRUDForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(178, 134);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(117, 58);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(437, 134);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(151, 58);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(178, 255);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(117, 62);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(437, 255);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(151, 62);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // CRUDForm
+            // 
+            this.ClientSize = new System.Drawing.Size(836, 442);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "CRUDForm";
+            this.Text = "CRUD Form";
+            this.Load += new System.EventHandler(this.CRUDForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ CRUDForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyMusicApp
+{
+    public partial class CRUDForm : Form
+    {
+        public CRUDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CRUDForm_Load(object sender, EventArgs e)
+        {
+            // Optional: Initialize default UI or load data here
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Create operation triggered.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Read operation triggered.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Update operation triggered.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Delete operation triggered.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
